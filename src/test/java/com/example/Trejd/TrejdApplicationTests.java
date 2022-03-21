cec4e117ae9eb2cf082b5c4daf5b84d4a7a9dd54
Simon <simonenholm@hotmail.com>
2022-03-21 15:35:47 +0100

    Initial commit

---
+++ src/test/java/com/example/Trejd/TrejdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trejd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrejdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
