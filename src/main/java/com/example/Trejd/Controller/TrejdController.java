3d0c1ef8c87715799d0ffeabc2e526d43a345286
Stina-Lise <Stina-Lise"stinalise.hullfors@gmail.com>
2022-03-25 10:03:13 +0100

    fixat review sida som bara kund och utförare har access till.

--- src/main/java/com/example/Trejd/Controller/TrejdController.java
+++ src/main/java/com/example/Trejd/Controller/TrejdController.java
@@ -1,14 +1,14 @@
 package com.example.Trejd.Controller;
 
+import com.example.Trejd.Review;
 import com.example.Trejd.Service.TrejdService;
+import com.example.Trejd.Trejd;
 import com.example.Trejd.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -19,11 +19,11 @@ public class TrejdController {
     @Autowired
     TrejdService service;
 
-  @GetMapping("/")
-  public String showStartPage() {
+    @GetMapping("/")
+    public String showStartPage() {
 
-    return "home";
-  }
+        return "home";
+    }
 
     @PostMapping("/test/{name}")
     public String getLoginPage(@RequestParam String email, @PathVariable String name, Model model, HttpSession session) {
@@ -40,27 +40,87 @@ public class TrejdController {
         // Sessions följer med under medans användaren är inloggad
         //session.getAttribute(user) -> Vid login. setSession;
         //session.setAttribute(user);
-        return  "home";
+        return "home";
     }
+
     @GetMapping("/login")
     public String getLoginPage2() {
         return "login";
     }
-    @PostMapping("/login")
-    public String checkLogin(@RequestParam String email, @RequestParam String password,HttpSession session) {
-        User user = service.getUser(email,password);
 
-      if(user!=null){
-          session.setAttribute("user",user);
-          return "my-page";
-      }
-      else{
-          System.out.println("No such user!");
-          return "login";
-      }
+    @PostMapping("/login")
+    public String checkLogin(@RequestParam String email, @RequestParam String password, HttpSession session) {
+        User user = service.getUser(email, password);
+
+        if (user != null) {
+            session.setAttribute("user", user);
+            return "my-page";
+        } else {
+            System.out.println("No such user!");
+            return "login";
+        }
     }
+
     @GetMapping("/my-page")
     public String getMyPage() {
         return "my-page";
+    }
+
+    @GetMapping("/info")
+    public String getInfo() {
+        return "info";
+    }
+
+    @GetMapping("/info2")
+    public String getInfo2() {
+        return "info2";
+    }
+
+    @GetMapping("/info3")
+    public String getInfo3() {
+        return "info3";
+    }
+
+    //Anropas när båda har avslutat trejden. När utföraren är klar och kund godkänt så skickas man till reviewn.
+
+    @GetMapping("/trejdInfo/{id}") //info om specifikt trejd-id
+    public String getTrejdInfo(@PathVariable Long id, Model model, HttpSession session) {
+        Trejd trejd = service.getTrejd(id); //vi plockar ut hela trejden från databasen baserat på id
+        User user = (User) session.getAttribute("user"); //vi plockar ut den inloggade användaren. getAttribute returnerar ett objekt. Den vet inte vad det är för typ, därför berättar vi det för den med (User). Man "castar".
+        if (user == null) { // dubbelkollar att någon är inloggad, är man inte det så redirectas man till loginsidan.
+            return "redirect:/login";
+        }
+
+        if (trejd.getOffer().getUser().getId() == user.getId()
+                || trejd.getOrder().getUser().getId() == user.getId()) { //vi kollar om den inloggade användaren är involverad i trejden
+
+            //vi går in i trejden. Vi kollar dens offer, sen kollar vi på offers user, sen kollar vi på users id, sen
+            //kollar vi om det är den som är inloggad
+            //vi går in i trejden. Vi kollar dens order, sen kollar vi på orderns user, sen kollar vi på users id, sen
+            //kollar vi om det är den som är inloggad. vi kollar om den som är inloggad är den som köpt eller den som erbjudit tjänsten
+
+            model.addAttribute("trejd", trejd); //vi lägger in trejden i modellen så att viewn kommer åt den
+            return "trejdInfo"; //vi kommer in på trejdInfo-sidan
+        } else {
+            return "redirect:/";//har personen inte tillgång till denna sida redirectas den till startsidan
+        }
+    }
+
+    @PostMapping("/trejdInfo/{trejdId}/addReview/")
+    public String addReview(@PathVariable Long trejdId, @RequestParam String description, @RequestParam int rating, HttpSession session) {
+        Trejd trejd = service.getTrejd(trejdId);
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/";
+        }
+
+        if (trejd.getOffer().getUser().getId() == user.getId()
+                || trejd.getOrder().getUser().getId() == user.getId()) {
+            Review review = new Review(trejd.getOffer().getUser(), trejd.getOrder().getUser(), trejd, description, rating);
+            service.createReview(review);
+            return "tack";
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/Trejd/Service/TrejdService.java
+++ src/main/java/com/example/Trejd/Service/TrejdService.java
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TrejdService {
@@ -141,6 +142,16 @@ public class TrejdService {
 
     public List<Skill> getAllSkillsByCategoryId(Long id) {
         return skillRepo.getAllSkillsByCategoryId(id);
+    }
+
+    //optional innebär att det kanske inte hittar trejden med det id:t och att vi kanske får andra alternativa grejer
+    public Trejd getTrejd(Long id) {
+        Optional<Trejd> trejd = trejdRepo.findById(id);
+        if (trejd.isPresent()) {
+            return trejd.get();
+        }else {
+            return null;
+        }
     }
 
 
